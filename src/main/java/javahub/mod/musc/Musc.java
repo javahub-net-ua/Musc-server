8a4e0fd9a681e290211dd04bc9938106325bc7ea
martin <nozistance@gmail.com>
2022-07-24 11:04:08 +0300

    Initial commit

---
+++ src/main/java/javahub/mod/musc/Musc.java
@@ -0,0 +1,10 @@
+package javahub.mod.musc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Musc implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/javahub/mod/musc/client/MuscClient.java
@@ -0,0 +1,13 @@
+package javahub.mod.musc.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MuscClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
